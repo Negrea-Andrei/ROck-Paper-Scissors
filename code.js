1e806426ae27363f4f11d3f1ac35602bff972358
Negrea-Andrei <negrea97@yahoo.com>
2023-01-26 21:03:41 +0200

    Created the code for the Rock-Paper-Scissor game

---
+++ code.js
@@ -0,0 +1,77 @@
+
+/*Make the computer pick a random choice */
+function getComputerChoice() {
+    let Choices = ['rock', 'paper', 'scissors'];
+
+    let rand = Math.floor(Math.random() * Choices.length);
+
+    return Choices[rand];
+}
+
+function round(ComputerChoices, PlayerChoice) {
+    if (ComputerChoices == PlayerChoice) {
+
+        PlayerScore = PlayerScore + 0;
+        ComputerScore = ComputerScore + 0;
+    }
+    /*Computer scores */
+    else if (ComputerChoices == 'rock' && PlayerChoice.toLowerCase() == 'scissors') {
+
+        ComputerScore = ComputerScore + 1;
+    }
+    else if (ComputerChoices == 'scissors' && PlayerChoice.toLowerCase() == 'paper') {
+
+        ComputerScore = ComputerScore + 1;
+    }
+    else if (ComputerChoices == 'paper' && PlayerChoice.toLowerCase() == 'rock') {
+
+        ComputerScore = ComputerScore + 1;
+    }
+
+    /*Players scores */
+    else {
+        PlayerScore++;
+    }
+}
+
+/*Set player score */
+let PlayerScore = 0;
+
+/*Set Computer score */
+let ComputerScore = 0;
+
+function PlayGame() {
+    /*Set a loop for 5 rounds */
+    for (i = 0; i < 5; i++) {
+
+        /*Set computer choice */
+        let ComputerChoices = getComputerChoice();
+
+        /*Set player choice */
+        let PlayerChoice = prompt("Please select rock, paper or scissors");
+
+        /*Compare the choices */
+        round(ComputerChoices, PlayerChoice)
+
+
+        /*Display the score and choices */
+        console.log('PlayerScore: ' + PlayerScore);
+        console.log(PlayerChoice);
+        console.log('ComputerScore: ' + ComputerScore);
+        console.log(ComputerChoices);
+    }
+}
+
+/*Execute the game */
+PlayGame();
+
+/*Declare the winner */
+if (PlayerScore > ComputerScore) {
+    alert("Player win!");
+}
+else if (PlayerScore < ComputerScore) {
+    alert("Player lost!");
+}
+else {
+    alert("It's a Draw!");
+}
