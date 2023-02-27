3dbe4e82967b3f28d449d8af48e76c48295b0c7e
Negrea-Andrei <negrea97@yahoo.com>
2023-02-27 21:42:12 +0200

    Implemented the code tu run when a button is clicked

--- code.js
+++ code.js
@@ -1,21 +1,31 @@
-/*Make the computer pick a random choice */
-function getComputerChoice() {
-    let Choices = ['rock', 'paper', 'scissors'];
-    let rand = Math.floor(Math.random() * Choices.length);
-    return Choices[rand];
-}
+/*Set player score */
+let PlayerScore = 0;
 
-const rock = document.querySelector("rock");
-const paper = document.querySelector("paper");
-const scissors = document.querySelector("scissors");
+/*Set Computer score */
+let ComputerScore = 0;
+
+const buttons = document.querySelectorAll('.choices button')
 
 
-function round(ComputerChoices, PlayerChoice) {
+function round(event) {
+
+    const PlayerChoice = event.target.className;
+
+    /*Make the computer pick a random choice */
+    function getComputerChoice() {
+        let Choices = ['rock', 'paper', 'scissors'];
+        let rand = Math.floor(Math.random() * Choices.length);
+        return Choices[rand];
+    }
+
+    /*Set computer choice */
+    let ComputerChoices = getComputerChoice();
 
     if (ComputerChoices == PlayerChoice) {
         PlayerScore = PlayerScore + 0;
         ComputerScore = ComputerScore + 0;
     }
+
     /*Computer scores */
     else if (ComputerChoices == 'rock' && PlayerChoice == 'scissors') {
         ComputerScore = ComputerScore + 1;
@@ -23,49 +33,31 @@ function round(ComputerChoices, PlayerChoice) {
     else if (ComputerChoices == 'scissors' && PlayerChoice == 'paper') {
         ComputerScore = ComputerScore + 1;
     }
-    else if (ComputerChoices == 'paper' && PlayerChoice == 'rock') {
+    else if (ComputerChoices == 'paper' && PlayerChoice== 'rock') {
         ComputerScore = ComputerScore + 1;
     }
+
     /*Players scores */
     else {
         PlayerScore++;
     }
+
     /*Display the score and choices */
     console.log('PlayerScore: ' + PlayerScore);
     console.log(PlayerChoice);
     console.log('ComputerScore: ' + ComputerScore);
     console.log(ComputerChoices);
+
+    /*Declare the winner */
+    if (PlayerScore > ComputerScore) {
+        alert("Player win!");
+    }
+    else if (PlayerScore < ComputerScore) {
+        alert("Player lost!");
+    }
+    else {
+        alert("It's a Draw!");
+    }
 }
 
-
-/*Set player score */
-let PlayerScore = 0;
-
-
-/*Set Computer score */
-let ComputerScore = 0;
-
-
-rock.addEventListener("click", () => {
-    round(getComputerChoice(), "rock")
-});
-
-paper.addEventListener("click", () => {
-     round(getComputerChoice(), "paper")
-});
-
-scissors.addEventListener("click", () => {
-    round(getComputerChoice(), "scissors")
-});      
-
-
-/*Declare the winner */
-if (PlayerScore > ComputerScore) {
-    alert("Player win!");
-}
-else if (PlayerScore < ComputerScore) {
-    alert("Player lost!");
-}
-else {
-    alert("It's a Draw!");
-}
+buttons.forEach(button => button.addEventListener('click', round));
